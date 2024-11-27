150fc9cb7bbbc6c05a1a07317475a149f8821eb2
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2024-11-27 11:40:14 +0000

    started header

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import streamlit as st
 import random
 import time
 import streamlit.components.v1 as components
+import base64
 
+LOGO_IMAGE = "images/NHS_England_logo.jpg"
 
 
 # Streamed response emulator
@@ -17,8 +19,24 @@ with open('./style.css') as f:
     css = f.read()
 st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
-st.title("Who are you?")
+st.image("images/NHS_England_logo.jpg", width=100)
+st.subheader("Workforce, training and education?")
 
+
+# st.markdown(
+#     f"""
+#     <div class="container">
+#         <img class="logo-img" src="data:image/png;base64,{base64.b64encode(open(LOGO_IMAGE, "rb").read()).decode()}">
+#         <p class="logo-text">Logo Much ?</p>
+#     </div>
+#     """,
+#     unsafe_allow_html=True
+# )
+
+
+
+st.subheader("Career coach")
+st.subheader("Who are you?")
 persona = st.selectbox(
     "Who are you?",
     (
@@ -30,11 +48,7 @@ persona = st.selectbox(
     placeholder="I am a...",
 )
 
-st.write("You selected:", persona)
-
-
-
-st.title("Let's chat")
+st.subheader("Let's chat")
 
 # Initialize chat history
 if "messages" not in st.session_state:
