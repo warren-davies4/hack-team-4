3a395f07c2e22d5338b6076bcf5c73139c9228e5
warren-davies4 <112185584+warren-davies4@users.noreply.github.com>
2024-11-27 11:59:15 +0000

    updated personas and responses

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ LOGO_IMAGE = "images/NHS_England_logo.jpg"
 
 
 # Streamed response emulator
-def response_generator(persona):
-    response = "you are a " + persona
+def response_generator(message, persona, language):
+    response = f"You said {message}, you are a {persona}, we are speaking in {language}"
     for word in response.split():
         yield word + " "
         time.sleep(0.05)
@@ -19,34 +19,47 @@ with open('./style.css') as f:
     css = f.read()
 st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
-st.image("images/NHS_England_logo.jpg", width=100)
-st.subheader("Workforce, training and education?")
 
 
-# st.markdown(
-#     f"""
-#     <div class="container">
-#         <img class="logo-img" src="data:image/png;base64,{base64.b64encode(open(LOGO_IMAGE, "rb").read()).decode()}">
-#         <p class="logo-text">Logo Much ?</p>
-#     </div>
-#     """,
-#     unsafe_allow_html=True
-# )
+st.markdown(
+    f"""
+    <div class="container">
+        <img class="logo-img" src="data:image/png;base64,{base64.b64encode(open(LOGO_IMAGE, "rb").read()).decode()}">
+        <p class="logo-text">Workforce, training and education</p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 
 
-st.subheader("Career coach")
+st.title("NHS Career coach")
 st.subheader("Who are you?")
+
+
+
 persona = st.selectbox(
     "Who are you?",
     (
-        "12 year old", 
-        "Doctor", 
+        "Child", 
+        "Adult",
+    ),
+    index=None,
+    placeholder="I am a...",
+)
+language = st.selectbox(
+    "Talk to me in...",
+    (
+        "English", 
+        "Spanish", 
+        "Korean",
         "Pirate"
     ),
     index=None,
     placeholder="I am a...",
 )
+
+
 
 st.subheader("Let's chat")
 
@@ -69,6 +82,6 @@ if prompt := st.chat_input("What is up?"):
 
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        response = st.write_stream(response_generator(persona))
+        response = st.write_stream(response_generator(st.session_state.messages[-1]['content'], persona, language))
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
